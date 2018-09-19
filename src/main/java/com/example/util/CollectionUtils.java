2e473f833f5acc743f864c7ce4fe56de918340a8
kuaik <359405466@qq.com>
2018-09-19 10:11:13 +0800

    打印完整sql

---
+++ src/main/java/com/example/util/CollectionUtils.java
@@ -0,0 +1,19 @@
+package com.example.util;
+
+import java.util.Collection;
+
+/**
+ * @Author: kuaik
+ * @Date: 2018-09-19
+ * @Description:
+ */
+public class CollectionUtils {
+
+    public static boolean isEmpty(Collection<?> collection){
+        return collection.isEmpty() || collection == null;
+    }
+
+    public static boolean isNotEmpty(Collection<?> collection){
+        return !isEmpty(collection);
+    }
+}
