dc40a5fbe14ecbd229cea83354f2d44a36405e9e
kuaik <359405466@qq.com>
2018-09-19 17:47:24 +0800

    IntegerCache

---
+++ src/main/java/com/example/controller/IntegerController.java
@@ -0,0 +1,15 @@
+package com.example.controller;
+
+/**
+ * @Author: kuaik
+ * @Date: 2018-09-19
+ * @Description:
+ */
+public class IntegerController {
+
+    public static void main(String[] args) {
+        Integer integer = Integer.valueOf(10);
+        Integer integer1 = new Integer(11);
+        System.out.println(integer);
+    }
+}
