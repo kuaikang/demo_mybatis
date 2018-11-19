db5ab7a5d192cb98e55b1626c8b49f53a22b444e
kuaikang <359405466@qq.com>
2018-11-19 17:26:18 +0800

    test

---
+++ src/main/java/com/example/util/Test.java
@@ -0,0 +1,15 @@
+package com.example.util;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author kuaikang
+ * @Date 2018/11/19 14:55:54
+ * @Version 1.0
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
