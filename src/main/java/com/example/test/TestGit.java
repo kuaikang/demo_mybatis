7114ac28479a6142bea8e2afc81182d0fc128d29
kuaikang <359405466@qq.com>
2018-12-17 14:27:46 +0800

    first commit

---
+++ src/main/java/com/example/test/TestGit.java
@@ -0,0 +1,15 @@
+package com.example.test;
+
+/**
+ * @ClassName TestGit
+ * @Description TODO
+ * @Author kuaikang
+ * @Date 2018/12/17 14:26:36
+ * @Version 1.0
+ */
+public class TestGit {
+
+    public void testReset(){
+        System.out.println("this is first commit push");
+    }
+}
